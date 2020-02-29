5d299bf772723a7d53cb1f2bc68f74846669032c
Philipp Wagner <philwagner@me.com>
2020-02-29 09:52:51 +0100

    add install dep script

---
+++ install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone -b zeus git://git.yoctoproject.org/poky
+git clone -b zeus git://git.openembedded.org/meta-openembedded
+git clone -b zeus git://git.yoctoproject.org/meta-raspberrypi
