19cc140c922970257d80e60eb6e30d7c8579bb02
Philipp Wagner <philwagner@me.com>
2020-02-29 10:15:11 +0100

    add first build setup script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,3 +2,4 @@
 git clone -b zeus git://git.yoctoproject.org/poky
 git clone -b zeus git://git.openembedded.org/meta-openembedded
 git clone -b zeus git://git.yoctoproject.org/meta-raspberrypi
+git clone -b zeus https://github.com/net-attack/meta-intel-realsense.git
